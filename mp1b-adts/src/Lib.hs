e5a2378521566122eddf3e81cc8ce12e7ab8982d
schmidtfrank <schmidtfrank190@gmail.com>
2026-02-15 23:08:08 +0000

    adts lists

--- mp1b-adts/src/Lib.hs
+++ mp1b-adts/src/Lib.hs
@@ -6,6 +6,7 @@
 
 module Lib where
 
+import Prelude as P
 --- Metadata for autograder
 --- -----------------------
 tag1 = 21923
@@ -30,27 +31,39 @@ data Exp = IntExp Integer
 --- ### list2cons
 
 -- don't forget to put the type declaration or you will lose points!
-list2cons = undefined
+list2cons :: [a] -> List a
+list2cons [] = Nil
+list2cons (x:xs) = Cons x (list2cons xs)
 
 --- ### cons2list
 
 -- don't forget to put the type declaration or you will lose points!
-cons2list = undefined
+cons2list :: List a -> [a]
+cons2list Nil = []
+cons2list (Cons a next) = a : cons2list next
 
 --- ### eval
 
 -- don't forget to put the type declaration or you will lose points!
-eval = undefined
+eval :: Exp -> Integer
+eval (IntExp num) = num
+eval (PlusExp []) = 0
+eval (MultExp []) = 1
+eval (PlusExp xs) = P.foldl (\acc x -> acc + x) 0 (P.map (eval) xs)
+eval (MultExp xs) = P.foldl (\acc x -> acc * x) 1 (P.map (eval) xs)
 
 --- ### list2cons'
 
 -- don't forget to put the type declaration or you will lose points!
-list2cons' = undefined
+list2cons' :: [a] -> List a
+list2cons' [] = Nil
+list2cons' (x:xs) = P.foldr (Cons) Nil (x:xs)
 
 --- ### BinTree
 
 -- BinTree
-
+data BinTree a = TreeNode a (BinTree a) (BinTree a) | Nil
+  deriving (Show, Eq)
 --- ### sumTree
 
 -- don't forget to put the type declaration or you will lose points!
